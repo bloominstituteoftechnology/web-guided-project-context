b1dbfbd677e7357902b55e0b75dfad817335235f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-04 13:47:52 -0500

    initial data.

--- review/src/index.js
+++ review/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render, useState } from 'react-dom';
 import './styles.scss';
+import data from './data';
 
 const App = ()=> {
+    const [person, setPerson] = useState();
     return(<h1>Hello World</h1>);
 };
 
