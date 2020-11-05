e62c541358b0d1c90928cde66aef4db2d0808d68
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 10:38:23 -0500

    adds App component.

---
+++ review/src/App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+const App = ()=> {
+    const [person, setPerson] = useState();
+    return(<h1>Hello World</h1>);
+};
+
+export default App;
--- review/src/index.js
+++ review/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { render, useState } from 'react-dom';
 import './styles.scss';
 import data from './data';
-
-const App = ()=> {
-    const [person, setPerson] = useState();
-    return(<h1>Hello World</h1>);
-};
+import App from './App';
 
 render(
     <App/>,
