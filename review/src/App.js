c290633c0771c9516f8b02c1b62f69532c4f6246
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-13 12:30:07 -0400

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -7,4 +7,18 @@ const App = ()=> {
     </div>);
 };
 
+const SubComp1 = ()=> {
+    return(<div className="component">
+        <h1>Sub Comp 1</h1>
+        <SubCom2 />
+    </div>);
+}
+
+const SubComp2 = ()=> {
+    return(<div className="component">
+        <h1>Sub Comp 2</h1>
+        <SubCom3 />
+    </div>);
+}
+
 export default App;
