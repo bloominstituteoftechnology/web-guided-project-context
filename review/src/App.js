926e63b2e9c6f3a6655194cc0c564425ae4d8d9a
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-12 10:22:12 -0400

    reset base guided project.

--- review/src/App.js
+++ review/src/App.js
@@ -3,22 +3,7 @@ import React from 'react';
 const App = ()=> {
     return(<div className="App component">
         <h1>Main App</h1>
-        <SubComp1 />
     </div>);
 };
-
-const SubComp1 = ()=> {
-    return(<div className="component">
-        <h1>Sub Comp 1</h1>
-        <SubComp2 />
-    </div>);
-}
-
-const SubComp2 = ()=> {
-    return(<div className="component">
-        <h1>Sub Comp 2</h1>
-        <SubComp3 />
-    </div>);
-}
 
 export default App;
