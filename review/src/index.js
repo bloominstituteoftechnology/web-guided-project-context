34d0f45f4b4ae4e8467f48eb8ecd705aa3fd415b
RinatTr <rinatreg@gmail.com>
2022-07-21 11:46:16 -0400

    work in progress

--- review/src/index.js
+++ review/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, useState } from 'react-dom';
+import { render } from 'react-dom';
 import './styles.scss';
 import data from './data';
 import App from './App';
